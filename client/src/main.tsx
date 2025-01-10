9ebf079b4a52b7a2f0d8a19e2e3667505aea123c
Vincent CHING <chingvincentdu20@yahoo.fr>
2025-01-10 09:33:25 +0100

    [FEAT] : Text About

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,11 +2,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import About from "./pages/About";
 
 /* ************************************************************************* */
-
-// Import the main app component
-import App from "./pages/App";
 
 // Import additional components for new routes
 // Try creating these components in the "pages" folder
@@ -19,11 +17,11 @@ import App from "./pages/App";
 // Create router configuration with routes
 // You can add more routes as you build out your app!
 const router = createBrowserRouter([
-  {
-    path: "/", // The root path
-    element: <App />, // Renders the App component for the home page
-  },
-  // Try adding a new route! For example, "/about" with an About component
+	{
+		path: "/About", // The root path
+		element: <About />, // Renders the App component for the home page
+	},
+	// Try adding a new route! For example, "/about" with an About component
 ]);
 
 /* ************************************************************************* */
@@ -31,14 +29,14 @@ const router = createBrowserRouter([
 // Find the root element in the HTML document
 const rootElement = document.getElementById("root");
 if (rootElement == null) {
-  throw new Error(`Your HTML Document should contain a <div id="root"></div>`);
+	throw new Error(`Your HTML Document should contain a <div id="root"></div>`);
 }
 
 // Render the app inside the root element
 createRoot(rootElement).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>,
 );
 
 /**
